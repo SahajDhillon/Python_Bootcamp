74d7f26f8ae8db8d283e1537b107ff0bc5492ec7
Sahaj <sahajdhillon225@gmail.com>
2023-04-17 19:37:23 +0530

    python_learning1.0

---
+++ Day1/Day1-printing-start.py
@@ -0,0 +1,5 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("Hello" + " Sahaj")
---
+++ Day2/Life_In_Weeks.py
@@ -0,0 +1,1 @@
+# You have 12410 days, 1768 Weeks, and 408 months left
